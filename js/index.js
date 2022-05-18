b077ea200a3348bb58bbf97c0a63cc69907d39ee
Jay Milledge <jm@jay-media.de>
2022-05-18 10:54:41 +0200

    Add sections

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ const showButton = document.querySelector('[data-js="show-button"]');
 const answerText = document.querySelector('[data-js="answer-container"]');
 const card = document.querySelector('[data-js="card-container"]');
 
+const homePage = document.querySelector('[data-js="home-page"]');
+const bookmarkPage = document.querySelector('[data-js="bookmark-page"]');
+const createPage = document.querySelector('[data-js="create-page"]');
+const profilePage = document.querySelector('[data-js="profile-page"]');
+
 function showAnswer() {
   showButton.addEventListener("click", () => {
     answerText.classList.toggle("answer-display");

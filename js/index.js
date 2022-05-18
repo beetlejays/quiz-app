70895c6ab9b008dfc96e9970c1ca6502f2374fce
Jay Milledge <jm@jay-media.de>
2022-05-18 21:01:57 +0200

    Upate buttons and js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
+/// Main Variables
+
 const showButton = document.querySelector('[data-js="show-button"]');
 const answerText = document.querySelector('[data-js="answer-container"]');
 const card = document.querySelector('[data-js="card-container"]');
-
+//// Page Variables
 const homePage = document.querySelector('[data-js="home-page"]');
 const bookmarkPage = document.querySelector('[data-js="bookmark-page"]');
 const createPage = document.querySelector('[data-js="create-page"]');
 const profilePage = document.querySelector('[data-js="profile-page"]');
+
+//// Page-Button Variables
+const homePageButton = document.querySelector('[data-js="home-page-button"]');
+
+const bookmarkPageButton = document.querySelector(
+  '[data-js="bookmark-page-button"]'
+);
+const createPageButton = document.querySelector(
+  '[data-js="create-page-button"]'
+);
+const profilePageButton = document.querySelector(
+  '[data-js="profile-page-button"]'
+);
+
+/// Functions
 
 function showAnswer() {
   showButton.addEventListener("click", () => {
@@ -14,5 +31,23 @@ function showAnswer() {
     card.classList.toggle("card--active");
   });
 }
+
+homePageButton.addEventListener("click", () => {
+  console.log("home");
+});
+
+createPageButton.addEventListener("click", () => {
+  console.log("create");
+});
+
+bookmarkPageButton.addEventListener("click", () => {
+  console.log("bookmark");
+});
+
+profilePageButton.addEventListener("click", () => {
+  console.log("profile");
+});
+
+//// Start functions
 
 showAnswer();

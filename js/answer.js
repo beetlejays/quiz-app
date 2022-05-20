a3aaec4837f101b365fe93adefef1cad981b6706
Jay Milledge <jm@jay-media.de>
2022-05-20 15:51:44 +0200

    Add foreach elemeents buttons

--- js/answer.js
+++ js/answer.js
@@ -1,13 +1,22 @@
 export function showAnswer() {
   /// Variables
-  const showButton = document.querySelector('[data-js="show-button"]');
+  const showButtons = document.querySelectorAll('[data-js="show-button"]');
   const answerText = document.querySelector('[data-js="answer-container"]');
   const card = document.querySelector('[data-js="card-container"]');
 
   /// Run functions
-  showButton.addEventListener('click', () => {
+  /*  showButton.addEventListener('click', () => {
     answerText.classList.toggle('answer-display');
     showButton.classList.toggle('primary-bg-color--active');
     card.classList.toggle('card--active');
+  }); */
+
+  showButtons.forEach(showButton => {
+    showButton.addEventListener('click', () => {
+      answerText.classList.toggle('answer-display');
+      showButton.classList.toggle('primary-bg-color--active');
+      card.classList.toggle('card--active');
+      console.log('button test');
+    });
   });
 }
